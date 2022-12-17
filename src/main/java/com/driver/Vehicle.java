4bad7d1eb69cd1b081a67282dd682f984b680c37
Swetha Shivani <cnk.swetha@gmail.com>
2022-12-17 15:12:49 +0530

    added getter and setter

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -1,10 +1,34 @@
 package com.driver;
 
 public class Vehicle {
-
     private String name;
     private int currentSpeed;
     private int currentDirection;
+    public java.lang.String getName() {
+        return name;
+    }
+
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+
+    public int getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public void setCurrentSpeed(int currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+
+    public int getCurrentDirection() {
+        return currentDirection;
+    }
+
+    public void setCurrentDirection(int currentDirection) {
+        this.currentDirection = currentDirection;
+    }
+
+
 
     public Vehicle(String name) {
         this.name = name;
