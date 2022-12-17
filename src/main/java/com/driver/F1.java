11e8e193e4ed6d618f6ade53010d25b50af7cf32
Swetha Shivani <cnk.swetha@gmail.com>
2022-12-17 14:43:59 +0530

    working

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -1,12 +1,14 @@
+
 package com.driver;
 
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name, 0, 0, 0, isManual, "", 0);
     }
 
-    public void accelerate(int rate){
+    public void accelerate(int rate) {
         int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
@@ -18,13 +20,45 @@ public class F1 extends Car {
          * speed more than 250: gear 6
          */
 
-        if(newSpeed == 0) {
+        newSpeed += rate;
+        if (newSpeed == 0) {
             //Stop the car, set gear as 1
+            changeSpeed(newSpeed, getCurrentDirection());
+            changeGear(1);
         }
+
         //for all other cases, change the gear accordingly
 
-        if(newSpeed > 0) {
+        if (newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
+            if (newSpeed > 0 && newSpeed < 51)
+                changeGear(1);
+            if (newSpeed > 50 && newSpeed < 101)
+                changeGear(2);
+            if (newSpeed > 100 && newSpeed < 151)
+                changeGear(3);
+            if (newSpeed > 150 && newSpeed < 201)
+                changeGear(4);
+            if (newSpeed > 200 && newSpeed < 251)
+                changeGear(5);
+            if (newSpeed > 250)
+                changeGear(6);
         }
     }
+
+    private int getCurrentDirection() {
+        return 0;
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -2,5 +2,38 @@ package com.driver;
 
 public interface WaterVehicle {
     public String getVehicleName();
+
     public int getVehicleCapacity();
+
+    public class Boat implements WaterVehicle {
+        String name;
+        int capacity;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getCapacity() {
+            return capacity;
+        }
+
+        public void setCapacity(int capacity) {
+            this.capacity = capacity;
+        }
+
+
+        @java.lang.Override
+        public String getVehicleName() {
+            return name;
+        }
+
+        @java.lang.Override
+        public int getVehicleCapacity() {
+            return capacity;
+        }
+    }
 }
